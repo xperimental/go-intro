a636daebedd636c5138382638f9bc9f0135c87db
Robert Jacob <robert.jacob@holidaycheck.com>
2018-02-21 12:12:24 +0100

    Second suggestions pass.

--- examples/flow/flow-switch-bare.go
+++ examples/flow/flow-switch-bare.go
@@ -10,15 +10,15 @@ func main() {
 	fileName := "file.txt"
 	// START OMIT
 	file, err := os.Open(fileName)
-	switch {
+	switch { // HL
 	case os.IsNotExist(err):
 		log.Fatalf("File does not exist: %s", fileName)
 	case err != nil:
 		log.Fatalf("Error opening file: %s", err)
 	default:
 	}
-	defer file.Close()
 	// END OMIT
+	defer file.Close()
 
 	// Show file
 	io.Copy(os.Stdout, file)
