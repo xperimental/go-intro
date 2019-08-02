c28b6f2835d121353bdd8659efaadd93dc02c45a
Robert Jacob <robert.jacob@holidaycheck.com>
2019-08-02 23:22:03 +0200

    Shorten main slides.

--- examples/goroutines/channels-for.go
+++ examples/goroutines/channels-for.go
@@ -2,35 +2,17 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
-var randomValue = rand.New(rand.NewSource(time.Now().Unix())).Intn
-
-// generator writes a number of random numbers to a channel.
-func generator(ch chan int, count int) {
-	for i := 0; i < count; i++ {
-		ch <- randomValue(100)
-	}
-	close(ch)
-}
-
-// START OMIT
-// createGenerator returns a read-only channel with some random integers.
-func createGenerator(count int) <-chan int { // HL
-	ch := make(chan int)
-	// start generating numbers
-	go generator(ch, count)
-	return ch
-}
-
 func main() {
-	numbersChannel := createGenerator(10)
+	messages := make(chan string, 2)
 
-	for n := range numbersChannel { // HL
-		fmt.Printf("We got a %d!\n", n)
+	messages <- "Hello"
+	messages <- "World"
+
+	// START OMIT
+	for msg := range messages { // HL
+		fmt.Println(msg)
 	}
+	// END OMIT
 }
-
-// END OMIT
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xperimental/go-intro
+
+go 1.12
